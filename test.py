aac695e4c0c91fbec804f43f62961e5ea839d5d6
Dominik Heidler <dheidler@gmail.com>
2012-12-19 18:05:39 +0100

    add test

---
+++ test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python2
+
+import dnsgeo
+
+print dnsgeo.lookup(hostname="bt-sa1-i.BT.DE.NET.DTAG.DE")
+print dnsgeo.lookup(hostname="xe-10-1-0.bar1.Munich1.Level3.net")
+print dnsgeo.lookup(hostname="er1.ams1.nl.above.net")
+
