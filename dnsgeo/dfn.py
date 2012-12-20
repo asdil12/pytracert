ab9cd5d81532736a5e81d6a3d8f767bc3791c51a
Dominik Heidler <dheidler@gmail.com>
2012-12-20 14:03:55 +0100

    add DFN

---
+++ dnsgeo/dfn.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+
+import base
+import exceptions
+
+import re
+import simplejson as json
+
+"""
+network: xr-bay1-te1-1.x-win.dfn.de
+
+"""
+
+class DFN(base.DNSGeoBase):
+	priority = 80
+	applre = re.compile(r"^\w*-(\w*)\d-.*\.x-win\.dfn\.de$")
+	def lookup(self, ip=None, hostname=None):
+		if hostname:
+			match = DFN.applre.match(hostname)
+			if match:
+				city_code = match.group(1)
+				returninfo = base.returninfo.copy()
+				jsondb = json.load(open('./database/dfn.json'))
+				c = jsondb[city_code.upper()]
+				returninfo['city'] = c['city']
+				returninfo['lat'] = c['lat']
+				returninfo['lng'] = c['lng']
+				returninfo['country'] = c['country']
+				returninfo['company'] = 'Deutschen Forschungsnetz'
+				returninfo['info'] = 'carrier'
+				return returninfo
+		raise exceptions.NotApplicable()
