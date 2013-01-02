24a00eb9bd3062cd621e7eee8d0a898034e40d86
Dominik Heidler <dheidler@gmail.com>
2013-01-02 16:05:32 +0100

    use IATA codes to get abovenet location

--- dnsgeo/abovenet.py
+++ dnsgeo/abovenet.py
@@ -13,12 +13,12 @@ network: ge-3-3-0.mpr1.%CITY1.%COUNTRY.above.net
 
 class AboveNet(base.DNSGeoBase):
 	priority = 80
-	applre = re.compile(r".*\.(\w*)\d\.(\w*)\.above\.net$")
+	applre = re.compile(r".*\.(\w*)\d\.(\w\w)\.above\.net$")
 	def lookup(self, ip=None, hostname=None):
 		if hostname:
 			match = AboveNet.applre.match(hostname)
 			if match:
-				city_code, country_code = match.group(1,2)
+				iata_code, country_code = match.group(1,2)
 				returninfo = base.returninfo.copy()
 				country = base.get_country_by_iso(country_code)
 				returninfo['country'] = country['name']
@@ -28,9 +28,8 @@ class AboveNet(base.DNSGeoBase):
 				returninfo['company'] = 'AboveNet Inc'
 				returninfo['info'] = 'carrier'
 				try:
-					jsondb = json.load(open('./database/carriers/abovenet.json'))
-					c = jsondb[city_code]
-					returninfo['city'] = c['city']
+					c = base.get_city_by_iata(iata_code)
+					returninfo['city'] = c['name']
 					returninfo['accuracy'] = 'city'
 					returninfo['lat'] = c['lat']
 					returninfo['lng'] = c['lng']
